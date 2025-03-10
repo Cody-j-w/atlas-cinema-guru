ae252f00675a7397b6ed30fb46dbcd53de15b69f
Cody Walenciak <codywalenciak@gmail.com>
2025-03-10 15:11:35 -0500

    Add header component layout

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,17 @@
 export default function Header() {
     return (
-        <div className="header">
-
+        <div className=" flex justify-between w-full bg-[#54F4D0] h-12">
+            <div className="inline content-center">
+                Logo goes here
+            </div>
+            <div className="flex flex-row ">
+                <div className="me-3 inline content-center">
+                    name goes here
+                </div>
+                <div className="me-3 inline content-center">
+                    <p className="">Logout goes here</p>
+                </div>
+            </div>
         </div>
     )
 }
