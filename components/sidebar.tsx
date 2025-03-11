edcf8686085c167ba95dd1d63b0d201085786516
Cody Walenciak <codywalenciak@gmail.com>
2025-03-11 16:22:28 -0500

    Add hover effect to sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
     return (
-        <div className="flex h-screen w-18 bg-[#1dd2af] place-self-start">
+        <div className="flex min-h-screen h-full w-18 hover:w-44 bg-[#1dd2af] place-self-start group">
             sidebar content
         </div>
     )
