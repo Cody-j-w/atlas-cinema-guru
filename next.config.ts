565151a3007c0f334d55f49f3fc67a0ba52f7070
Cody Walenciak <codywalenciak@gmail.com>
2025-03-14 14:45:21 -0500

    Add redirect

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/1',
+        permanent: true
+      }
+
+    ]
+  }
 };
 
 export default nextConfig;
