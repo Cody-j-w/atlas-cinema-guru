9e6fc26bf5aa8c4df67a8a5886b16393cb4a0324
Cody Walenciak <codywalenciak@gmail.com>
2025-03-15 16:22:00 -0500

    Pivot from dynamic routing to searchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
-export default async function Page() {
+import FilmDisplay from "@/components/filmDisplay";
+import FilmImage from "@/components/filmImage";
+import SearchField from "@/components/searchField";
 
+export default async function Page(props: {
+  searchParams?: Promise<{
+    page?: string;
+    minYear?: string;
+    maxYear?: string;
+    query?: string;
+    genres?: string[]
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const currentPage = searchParams?.page || "1";
+  return (
+    <div className="flex flex-wrap items-center justify-center min-h-screen h-full w-full">
+      <SearchField />
+      <FilmDisplay page={currentPage} />
+    </div>
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/1',
-        permanent: true
-      }
-
-    ]
   }
 };
 
