2854f3954546f95d2f7e097bbcb071057c714d81
Cody Walenciak <codywalenciak@gmail.com>
2025-03-17 14:03:49 -0500

    Add reactivity to favoriteButtons component

--- components/favoriteButtons.tsx
+++ components/favoriteButtons.tsx
@@ -33,18 +33,18 @@ export default async function FavoriteButtons(props: { id: string, favorited: bo
         revalidatePath('/')
     }
     return (
-        <div className="absolute flex flex-row top-2 right-2">
+        <div className="absolute hidden group-hover:flex flex-row top-2 right-2">
             <form className="me-2" action={handleFavorite}>
                 <input name="favorited" className="hidden" readOnly value={props.favorited.toString()} />
                 <input name="id" className="hidden" readOnly value={props.id} />
-                <button type="submit">
+                <button className="cursor-pointer" type="submit">
                     {props.favorited ? <Star fill="white" stroke="0" /> : <Star />}
                 </button>
             </form>
             <form className="me-1" action={handleWatchLater}>
                 <input name="watchLater" className="hidden" readOnly value={props.watchLater.toString()} />
                 <input name="id" className="hidden" readOnly value={props.id} />
-                <button type="submit" className="">
+                <button className="cursor-pointer" type="submit">
                     {props.watchLater ? <Clock color="black" fill="white" /> : <Clock />}
                 </button>
             </form>
