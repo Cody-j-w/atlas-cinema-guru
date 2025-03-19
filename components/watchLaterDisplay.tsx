43090c8f44972a61e69d67cff9b2277af599731a
Cody Walenciak <codywalenciak@gmail.com>
2025-03-19 15:05:23 -0500

    Add favorites/watch later headers

--- components/watchLaterDisplay.tsx
+++ components/watchLaterDisplay.tsx
@@ -12,6 +12,7 @@ export default async function WatchLatersDisplay({ page }: {
     const nextPageContent = await fetchWatchLaters(parseInt(page) + 1, session?.user?.email!!)
     return (
         <div className="flex flex-wrap items-center justify-center min-h-screen h-full w-full">
+            <h1 className="w-full text-center text-6xl font-bold">Watch Later</h1>
             {films.map((film) => (
                 <FilmImage key={film.id} id={film.id} title={film.title} released={film.released} genre={film.genre} synopsis={film.synopsis} favorited={film.favorited} watchLater={film.watchLater} image={film.image} />
             ))}
